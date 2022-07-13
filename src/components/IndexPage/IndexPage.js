ad602739b510c1b4569d14ebc34bb1b1beda327e
Brando <leukocyte@protonmail.com>
2022-07-13 16:33:46 -0500

    index page random picture generator

--- src/components/IndexPage/IndexPage.js
+++ src/components/IndexPage/IndexPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './IndexPage.css'
-const Car = require('./indexpic01.jpg');
-const Snow = require('./indexpic02.jpg');
 
 const IndexPage = () => {
-
+  
+  const Car = require('./indexpic01.jpg');
+  const Factory = require('./indexpic02.jpg');
+  const Beach = require('./indexpic03.jpg');
+  const Woods = require('./indexpic04.jpg');
   const images = [];
-  images.push(Car, Snow);
+  images.push(Car, Factory, Beach, Woods);
   
 
 
@@ -16,7 +18,7 @@ const IndexPage = () => {
       <Header/>
 
       <div className='index-img'>
-      <img src={images[Math.floor(Math.random()*2)]} alt="Stereotypical beat up old car" />
+      <img src={images[Math.floor(Math.random()*4)]} alt="Stereotypical beat up old car" />
       </div>
 
     </div>
